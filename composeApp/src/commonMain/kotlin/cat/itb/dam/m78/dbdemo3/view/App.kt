7da2155cad52f8d6b39946ea363ff48c299cb5ea
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-04-02 13:00:10 +0200

    creado

---
+++ composeApp/src/commonMain/kotlin/cat/itb/dam/m78/dbdemo3/view/App.kt
@@ -0,0 +1,90 @@
+package cat.itb.dam.m78.dbdemo3.view
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cat.itb.dam.m78.dbdemo3.model.DatabaseViewModel
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App(viewModel: DatabaseViewModel=DatabaseViewModel()) {
+    MaterialTheme {
+
+        //Llista amb tots els registres, obtinguda del ViewModel
+        val all = viewModel.allTexts.value
+        var inputText by remember { mutableStateOf("") }
+
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            // Text field and button
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(8.dp))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    //El textFiel està enllaçat al camp inputText.  No està al ViewModel per què és funcionament de la pantalla
+                    TextField(
+                        value = inputText,
+                        onValueChange = { inputText = it },
+                        label = { Text("Enter text") },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(8.dp)
+                            .background(MaterialTheme.colors.background, shape = RoundedCornerShape(8.dp))
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        // Quanes fa click, el botó cirda al ViewModel per fer un insert a la base de dades
+                        onClick = { viewModel.insertText(inputText) },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Add", color = MaterialTheme.colors.onPrimary)
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+//sd
+            // List of items
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(all) { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(8.dp))
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(item.text, style = MaterialTheme.typography.body1)
+                        IconButton(onClick = {viewModel.deleteText(item.id) }) {
+                            Icon(Icons.Default.Delete, contentDescription = "Delete")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
